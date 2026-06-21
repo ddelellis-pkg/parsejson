91df80b2f7ecf5af29c5526f7154d83015ff42a7
Daniel De Lellis <dan.delellis@gmail.com>
2026-06-21 16:18:05 -0700

    describe why the failure happened

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package parsejson
 import (
+	"fmt"
 	"os"
 	"encoding/json"
 )
@@ -8,9 +9,12 @@ import (
 func MapOfStrings(path string) (guy map[string]string, err error) {
 	var data []byte
 	if data, err = os.ReadFile(path); err != nil {
+		err = fmt.Errorf("Failed parsing %s: %w", path, err)
 		return
 	}
 	guy = make(map[string]string)
-	err = json.Unmarshal(data, &guy)
+	if err = json.Unmarshal(data, &guy); err != nil {
+		err = fmt.Errorf("Failed marshalling data to struct %T: %w", guy, err)
+	}
 	return
 }
